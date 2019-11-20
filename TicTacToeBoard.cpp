676fd9c6ae398e4073bc14ccbafbb2a669c0a986
kyle rodgers <kyle77r@gmail.com>
2019-11-20 15:44:45 -0800

    Added extra check to get rid of warnings

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -67,16 +67,16 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 Piece TicTacToeBoard::getWinner()
 {
   for (int i=0; i<3; i++) {
-    if (board[i][0]!=Blank && (board[i][0]==board[i][1]==board[i][2]))
+    if (board[i][0]!=Blank && (board[i][0]==board[i][1] && board[i][1]==board[i][2]))
       return board[i][0];
   }
   for (int i=0; i<3; i++) {
-    if (board[0][i]!=Blank && (board[0][i]==board[1][i]==board[2][i]))
+    if (board[0][i]!=Blank && (board[0][i]==board[1][i] && board[0][i]==board[2][i]))
       return board[0][i];
   }
-  if (board[0][0]!=Blank && board[0][0]==board[1][1]==board[2][2])
+  if (board[0][0]!=Blank && board[0][0]==board[1][1] && board[0][0]==board[2][2])
     return board[0][0];
-  else if (board[0][2]!=Blank && board[0][2]==board[1][1]==board[2][0])
+  else if (board[0][2]!=Blank && board[0][2]==board[1][1] && board[0][2]==board[2][0])
     return board[0][2];
   return Invalid;
 }
